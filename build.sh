3b6ac8fb3e03e10c57d3f728ba252e25519c9551
arttttt <artem-bambalov@yandex.ru>
2017-09-13 17:31:06 +0300

    mocha:upgrade build.sh

--- build.sh
+++ build.sh
@@ -1,49 +1,182 @@
 #!/bin/bash
-R=$(pwd)
-export ARCH=arm
-DEFCONFIG=mocha_android_defconfig
-CROSS_COMPILER=$R/tc/bin/arm-linux-gnueabihf- 
-OUT_DIR=$R/out
-BUILDING_DIR=$OUT_DIR/kernel_obj
-MODULES_DIR=$OUT_DIR/modules
-JOBS=8 # x Number of cores
 
-mkdir -p $OUT_DIR $BUILDING_DIR $MODULES_DIR
-FUNC_CLEANUP()
-{
-	echo -e "\n\e[95mCleaning up..."
-	mkdir -p $OUT_DIR $BUILDING_DIR $MODULES_DIR
-	echo -e "\e[34mAll clean!"
+export ARCH="arm"
+export KBUILD_BUILD_HOST="kernel v2.0"
+export KBUILD_BUILD_USER="Dargons"
+
+clean_build=0
+config="tegra12_android_defconfig"
+dtb_name="tegra124-mocha.dtb"
+dtb_only=0
+kernel_name=$(git rev-parse --abbrev-ref HEAD)
+threads=5
+toolchain="$HOME/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+
+KERNEL_DIR=$PWD
+ORIGINAL_OUTPUT_DIR="$KERNEL_DIR/arch/$ARCH/boot"
+OUTPUT_DIR="$KERNEL_DIR/output"
+
+ERROR=0
+HEAD=1
+WARNING=2
+
+function printfc() {
+	if [[ $2 == $ERROR ]]; then
+		printf "\e[1;31m$1\e[0m"
+		return
+	fi;
+	if [[ $2 == $HEAD ]]; then
+		printf "\e[1;32m$1\e[0m"
+		return
+	fi;
+	if [[ $2 == $WARNING ]]; then
+		printf "\e[1;35m$1\e[0m"
+		return
+	fi;
 }
 
-FUNC_COMPILE()
+function generate_version()
 {
-	echo -e "\n\e[95mStarting the build..."
-	make -C $R O=$BUILDING_DIR $DEFCONFIG 
-	make -j$JOBS -C $R O=$BUILDING_DIR ARCH=arm CROSS_COMPILE=$CROSS_COMPILER
-	make tegra124-mocha.dtb -C $R O=$BUILDING_DIR ARCH=arm CROSS_COMPILE=$CROSS_COMPILER
-	cp $OUT_DIR/kernel_obj/arch/arm/boot/zImage $OUT_DIR/zImage
-	cp $OUT_DIR/kernel_obj/arch/arm/boot/dts/tegra124-mocha.dtb $OUT_DIR/mocha.dtb
-	echo -e "\e[34mJob done!"
+	if [[ -f "$KERNEL_DIR/.git/HEAD"  &&  -f "$KERNEL_DIR/anykernel/anykernel.sh" ]]; then
+		local updated_kernel_name
+		eval "$(awk -F"="  '/kernel.string/{print "anykernel_name="$2}' $HOME/kernel/anykernel/anykernel.sh)" 
+		eval "$(echo $kernel_name | awk -F"-"  '{print "current_branch="$2}')"
+		if [[ "$current_branch" != "stable" ]]; then
+			if [[ ! -f "$KERNEL_DIR/version" ]]; then
+				echo "build_number=0" > $KERNEL_DIR/version
+			fi;
 
-	echo -e "\n\e[95mCopying the Modules..."
-	rm -rf $MODULES_DIR
-	mkdir $MODULES_DIR
-	find . -name "*.ko" -exec cp {} $MODULES_DIR \;
-	echo -e "\e[34mDone!"
+			awk -F"="  '{$2+=1; print $1"="$2}' $KERNEL_DIR/version > tmpfile
+			mv tmpfile $KERNEL_DIR/version
+			eval "$(awk -F"="  '{print "current_build="$2}' $KERNEL_DIR/version)"
+			export LOCALVERSION="-$current_branch-build$current_build"
+			updated_kernel_name=$kernel_name"-build"$current_build
+		else
+			updated_kernel_name=$kernel_name
+		fi;
+			sed -i s/$anykernel_name/$updated_kernel_name/ $KERNEL_DIR/anykernel/anykernel.sh
+	fi;
 }
 
-echo -e -n "\e[33mDo you want to clean build directory (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty $old_stty_cfg
+function make_zip()
+{
+	if [[ -d "$KERNEL_DIR/anykernel" ]]; then
+		printfc "\nСоздание zip архива\n\n" $HEAD
+	else
+		printfc "\nПапка $KERNEL_DIR/anykernel не существует\n\n" $ERROR
+		return
+	fi;
 
-if echo "$answer" | grep -iq "^y" ;then
-    FUNC_CLEANUP
-    FUNC_COMPILE
-else
-    rm -r $OUT_DIR/zImage
-    FUNC_COMPILE
-    
-fi
+	if [[ -f "$ORIGINAL_OUTPUT_DIR/zImage" ]]; then
+		mv $ORIGINAL_OUTPUT_DIR/zImage $PWD/anykernel/kernel/
+	else
+		if [[ $dtb_only == 0 ]]; then
+			printfc "Файл $ORIGINAL_OUTPUT_DIR/zImage не существует\n\n" $ERROR
+			return
+		fi
+	fi
+
+	if [[ -f "$ORIGINAL_OUTPUT_DIR/dts/$dtb_name" ]]; then
+		mv $ORIGINAL_OUTPUT_DIR/dts/$dtb_name $PWD/anykernel/kernel/dtb
+	else
+		if [[ $dtb_only == 0 ]]; then
+			printfc "Файл $ORIGINAL_OUTPUT_DIR/dts/$dtb_name не существует\n\n" $ERROR
+			return
+		fi
+	fi
+
+	cd $KERNEL_DIR/anykernel
+	local zip_name="$kernel_name($(date +'%d.%m.%Y-%H.%M')).zip"
+	zip -r $zip_name *
+
+	if [[ -f "$PWD/$zip_name" ]]; then
+		if [[ ! -d "$OUTPUT_DIR" ]]; then
+			mkdir $OUTPUT_DIR
+		fi;
+
+		printfc "\n$zip_name создан, перемещение в $OUTPUT_DIR" $HEAD
+		mv "$PWD/$zip_name" $OUTPUT_DIR
+
+		if [[ -f "$OUTPUT_DIR/$zip_name" ]]; then
+			echo
+			printfc "\nЗавершено\n" $HEAD
+		fi
+	else
+		printfc "\nНе удалось создать архив\n" $ERROR
+		return
+	fi
+	cd $KERNEL_DIR
+}
+
+function compile()
+{
+	local start=$(date +%s)
+	clear
+
+	if [[ "$clean_build" == 1 ]]; then
+		make clean
+		make mrproper
+	fi
+
+	generate_version
+	make $config
+	make -j$threads ARCH=$ARCH CROSS_COMPILE=$toolchain zImage
+
+	printfc "\nКомпиляция дерева устройства\n\n" $HEAD
+
+	make -j$threads ARCH=$ARCH CROSS_COMPILE=$toolchain $dtb_name
+
+	local end=$(date +%s)
+	local comp_time=$((end-start))
+	printf "\e[1;32m\nЯдро скомпилировано за %02d:%02d\n\e[0m" $((($comp_time/60)%60)) $(($comp_time%60))
+	printfc "Сборка номер $current_build в ветке $current_branch\n" $HEAD
+
+	make_zip
+}
+
+function compile_dtb()
+{
+	clear
+
+	dtb_only=1
+	generate_version
+	make $config
+	make -j$threads ARCH=$ARCH CROSS_COMPILE=$toolchain $dtb_name
+
+	if [[ -f "$ORIGINAL_OUTPUT_DIR/dts/$dtb_name" ]]; then
+		mv $ORIGINAL_OUTPUT_DIR/dts/$dtb_name $PWD/anykernel/kernel/dtb
+	else
+		printfc "Файл $ORIGINAL_OUTPUT_DIR/dts/$dtb_name не существует\n\n" $ERROR
+		return
+	fi
+
+	make_zip
+}
+
+function main()
+{
+	clear
+	echo "---------------------------------------------------"
+	echo "Выполнить чистую сборку?                          -"
+	echo "---------------------------------------------------"
+	echo "1 -clean_build=1;compile;;                                           -"
+	echo "---------------------------------------------------"
+	echo "2 - compile;;                                       -"
+	echo "---------------------------------------------------"
+	echo "3 - compile dtb                                   -"
+	echo "---------------------------------------------------"
+	echo "4 - return                                         -"
+	echo "---------------------------------------------------"
+	printf %s "Ваш выбор: "
+	read env
+
+	case $env in
+		1) clean_build=1;compile;;
+		2) compile;;
+		3) compile_dtb;;
+		4) clear;return;;
+		*) main;;
+	esac
+}
+
+main
