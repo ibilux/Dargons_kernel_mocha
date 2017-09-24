0c7eb5373c33172e1d262dbb9099e5647ab1a7f5
TheVirus! <kingmouryarahul@gmail.com>
2017-09-24 11:35:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ CROSS_COMPILER=$R/tc/bin/arm-linux-gnueabihf-
 OUT_DIR=$R/out
 BUILDING_DIR=$OUT_DIR/kernel_obj
 MODULES_DIR=$OUT_DIR/modules
-ANYKERNEL_DIR=$R/AnyKernel2
 JOBS=16 #2 x Number of cores
 
 mkdir -p $OUT_DIR $BUILDING_DIR $MODULES_DIR
