63d1bc37b72c4810402935313b733ec5d39fd3ed
TheVirus! <kingmouryarahul@gmail.com>
2017-09-24 04:02:00 +0000

    Adding build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+R=$(pwd)
+
+export ARCH=arm
+export CROSS_COMPILE=$R/tc/bin/arm-linux-gnueabihf- 
---
+++ toolchain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/RahulTheVirus/toolchain.git tc
+
+sudo find tc -type f -exec chmod a+rwx {} \;
+
+fi
