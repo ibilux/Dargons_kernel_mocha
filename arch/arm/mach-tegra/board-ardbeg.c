725a8128d4e3793ee87fe7921a1aa8d11f189b9e
Artyom Bambalov <artem-bambalov@yandex.ru>
2019-07-28 23:53:57 +0300

    Revert "arm: tegra: remove touch init from ardbeg board"
    This reverts commit 302e4c136939a0fe40197959b66beeb2e3345e64.

--- arch/arm/mach-tegra/board-ardbeg.c
+++ arch/arm/mach-tegra/board-ardbeg.c
@@ -78,6 +78,7 @@
 #include <asm/mach/arch.h>
 #include <mach/gpio-tegra.h>
 #include <mach/xusb.h>
+#include <linux/i2c/atmel_mxt_ts.h>
 
 #include <linux/firmware.h>
 
@@ -894,6 +895,98 @@ static void __init edp_init(void)
 	}
 }
 
+#ifdef CONFIG_TOUCHSCREEN_ATMEL_MXT
+
+#define TP_GPIO_RESET			TEGRA_GPIO_PK4
+#define TP_GPIO_INTR			TEGRA_GPIO_PR7
+ 
+static int mxt_lens_1664t_key_codes[MXT_KEYARRAY_MAX_KEYS] = {
+ 	KEY_BACK, KEY_HOME, KEY_MENU, KEY_POWER,
+};
+
+static int mxt_lens_1066t_key_codes[MXT_KEYARRAY_MAX_KEYS] = {
+ 	KEY_MENU, KEY_HOME, KEY_BACK, KEY_POWER,
+};
+ 
+static struct mxt_config_info mxt_config_array[] = {
+ 	{
+ 		.family_id	= 0xA4,
+ 		.variant_id	= 0x04,
+ 		.version	= 0x10,
+ 		.build		= 0xAA,
+ 		.user_id	= 0x00,
+ 		.bootldr_id	= 0x48,
+ 		.mxt_cfg_name	= "mxt1664t_config_no_dummy.fw",
+ 		.vendor_id	= 0x4,
+ 		.key_codes		= mxt_lens_1664t_key_codes,
+ 		.key_num		= 4,
+ 		.mxt_fw_name		= "mxt1664t.fw",
+ 	},
+ 	{
+ 		.family_id	= 0xA4,
+ 		.variant_id	= 0x04,
+ 		.version	= 0x10,
+ 		.build		= 0xAA,
+ 		.user_id	= 0xAA,
+ 		.bootldr_id	= 0x48,
+ 		.mxt_cfg_name	= "mxt1664t_config_with_dummy.fw",
+ 		.vendor_id	= 0x4,
+ 		.key_codes		= mxt_lens_1664t_key_codes,
+ 		.key_num		= 4,
+ 		.mxt_fw_name		= "mxt1664t.fw",
+ 	},
+ 	{
+ 		.family_id	= 0xA4,
+ 		.variant_id	= 0x0B,
+ 		.version	= 0x12,
+ 		.build		= 0xAA,
+ 		.user_id	= 0x00,
+ 		.bootldr_id	= 0x51,
+ 		.mxt_cfg_name	= "mxt1066t_config.fw",
+ 		.vendor_id	= 0x4,
+ 		.key_codes		= mxt_lens_1066t_key_codes,
+ 		.key_num		= 4,
+ 		.mxt_fw_name		= "mxt1066t.fw",
+ 	},
+};
+
+static struct mxt_platform_data mxt_platform_data = {
+ 	.config_array		= mxt_config_array,
+ 	.config_array_size		= ARRAY_SIZE(mxt_config_array),
+ 	.irqflags		= IRQF_TRIGGER_LOW | IRQF_ONESHOT,
+ 	.power_gpio		= -1,
+ 	.reset_gpio		= TP_GPIO_RESET,
+ 	.irq_gpio		= TP_GPIO_INTR,
+ 	.gpio_mask		= 0xc,
+ 	.vendor_info		= 0x03eb,
+ 	.product_info		= 0x214f,
+ };
+ 
+static struct i2c_board_info mxt_device_info[] __initdata = {
+ 	{
+ 		I2C_BOARD_INFO("atmel_mxt_ts", 0x4a),
+ 		.platform_data = &mxt_platform_data,
+ 	},
+};
+ 
+#endif
+
+static int __init ardbeg_touch_init(void)
+{
+#ifdef CONFIG_TOUCHSCREEN_ATMEL_MXT	
+ 	int i;
+ 	
+ 	pr_info(" %s init atmel touch\n", __func__);
+ 	for (i = 0; i < ARRAY_SIZE(mxt_device_info); i++) {
+ 		mxt_device_info[i].irq = gpio_to_irq(TP_GPIO_INTR);
+ 	}		
+ 
+ 	i2c_register_board_info(3, mxt_device_info, ARRAY_SIZE(mxt_device_info));			
+#endif	
+
+	return 0;
+}
+
 static void __init tegra_ardbeg_early_init(void)
 {
 	ardbeg_sysedp_init();
@@ -1087,6 +1180,8 @@ static void __init tegra_ardbeg_late_init(void)
 
 	edp_init();
 	isomgr_init();
+	ardbeg_touch_init();
+
 
 	if (board_info.board_id == BOARD_E2548 ||
 			board_info.board_id == BOARD_P2530)
